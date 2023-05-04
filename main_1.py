37fdf1c743ae4e72df8510cde85bd2dc42fd7158
lokesh kumar <57664015+lokeshllkumar@users.noreply.github.com>
2023-05-04 22:04:59 +0530

    Update main_1.py

--- main_1.py
+++ main_1.py
@@ -2,92 +2,58 @@
 pip install opencv-python
 numpy is preinstalled 
 pip install matplotlib
-pip install -U scikit-learn
+pip install mediapipe
 '''
 
 import cv2 as cv
+import mediapipe as mp
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+
+mp_draw=mp.solutions.drawing_utils
+mp_pose=mp.solutions.pose
+pose=mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5)
+
+stat=str()
 
 cap=cv.VideoCapture(0)
-ct=0
-bg=cv.createBackgroundSubtractorMOG2()
-stat=None
-
-loss=[]
-
-t_label,p_label=[],[]
-
-num_c,ac_list=0,[]
+cap.set(cv.CAP_PROP_FRAME_WIDTH,1920)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT,1080)
 
 while True:
     frame=cv.flip(cap.read()[1],1)
-    fgmask=bg.apply(cv.cvtColor(frame,cv.COLOR_BGR2GRAY))
-    contours=cv.findContours(image=fgmask,mode=cv.RETR_TREE,method=cv.CHAIN_APPROX_NONE)[0]
-
-    thresh=cv.threshold(cv.cvtColor(frame,cv.COLOR_BGR2GRAY),127,255,cv.THRESH_BINARY)[1]
-    kernel=cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
-    op=cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel)
-    cl=cv.morphologyEx(op,cv.MORPH_CLOSE,kernel)
-    sk=np.zeros(cl.shape,np.uint8)
-    el=cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
-    stat=True
-    while stat:
-        eroded=cv.erode(cl,el)
-        temp=cv.dilate(eroded,el)
-        temp=cv.subtract(cl,temp)
-        sk=cv.bitwise_or(sk,temp)
-        if cv.countNonZero(cl:=eroded.copy())==0:
-            stat=False
-    cv.imshow('skel',sk)
-    if contours:
-        ar=[]
-        for contour in contours:
-            ar.append(cv.contourArea(contour))
-        M=cv.moments(contours[ar.index(max(ar,default=0))])
-        x,y,w,h=cv.boundingRect(contours[ar.index(max(ar,default=0))])
-        cv.drawContours(fgmask,[contours[ar.index(max(ar,default=0))]],0,(255,255,255),1,maxLevel=0)
-        a_ratio=w/h
-        color=(0,255,0)
-        if 0.9<=a_ratio<=1.1:
-            stat='STANDING'
-            loss.append(0)
-            t_label.append('STANDING')
-            num_c+=1
-        elif a_ratio>1.1:
-            stat='LYING'
-            color=(0,0,255)
-            loss.append(1)
-            t_label.append('LYING')
-        else:
-            stat='SITTING'
-            loss.append(2)
-            t_label.append('SITTING')
-            num_c+=1
-        p_label.append(stat)
-    cv.rectangle(frame,(x,y),(x+w,y+h),color,2)
-    cv.putText(frame,stat,(x,y),cv.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),2)
-    cv.imshow('bruh',frame)    
+    try:
+        res=pose.process(cv.cvtColor(frame,cv.COLOR_BGR2RGB))
+        landmarks=res.pose_landmarks
+        if landmarks is not None:
+            x_min=frame.shape[1]
+            y_min=frame.shape[0]
+            x_max=0
+            y_max=0
+            for landmark in landmarks.landmark:
+                x,y=int(landmark.x*frame.shape[1]),int(landmark.y*frame.shape[0])
+                if x<x_min:
+                    x_min=x
+                if x>x_max:
+                    x_max=x
+                if y<y_min:
+                    y_min=y
+                if y>y_max:
+                    y_max=y
+            cv.rectangle(frame,(x_min,y_min),(x_max,y_max),(255,0,0),2)
+            w,h=x_max-x_min,y_max-y_min
+            a_ratio=w/h
+            if a_ratio<=0.4:
+                stat='STANDING'
+            elif a_ratio>2:
+                stat='LYING DOWN'
+            else:
+                stat='SITTING'
+            cv.putText(frame,stat,(x_min,y_min),cv.FONT_HERSHEY_COMPLEX,0.5,(0,0,0),2)
+        mp_draw.draw_landmarks(frame,res.pose_landmarks,mp_pose.POSE_CONNECTIONS)
+        cv.imshow('final',frame)
+    except:
+        break
     if cv.waitKey(1) & 0xFF==ord('q'):
         break
-    ct+=1
-    ac_list.append(num_c/ct)
 cap.release()
 cv.destroyAllWindows()
-
-plt.plot(loss)
-plt.title('loss graph')
-plt.xlabel('frame')
-plt.ylabel('loss')
-plt.show()
-
-plt.plot(ac_list)
-plt.title('accuracy graph')
-plt.xlabel('frame')
-plt.ylabel('accuracy')
-plt.show()
-
-c_mat=confusion_matrix(t_label,p_label,labels=['STANDING','LYING','SITTING'])
-print("confusion matrix:")
-print(c_mat)
